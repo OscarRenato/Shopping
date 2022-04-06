c41d83013984fef5221d14750a29e015863469a1
Oscar Gomez <oscarrenato@yahoo.com>
2022-04-05 20:55:44 -0500

    Entities Country and Categories

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        //public ICollection<State> States { get; set; }
+
+        //[Display(Name = "Departamentos/Estados")]
+        //public int StatesNumber => States == null ? 0 : States.Count;
+    }
+}
